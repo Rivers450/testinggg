7f78e8bac3789c71d806d9dd156a56007b484151
Rivers450 <rivers234@revature.net>
2024-04-25 11:48:05 -0400

    discussing different vars and scopes

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 /**
  * just a random comment
  */
+
+console.log('hello world');
+
+
+var myVariable; //either global or function scope depending on where it is
+
+let myVariable; //has block scope
+
+const myVariable = 3; //has block block scope
+
+/*
+block scope = we can reference an item where it is
+declared and any inner code blocks
+
+function scope = we can reference an item within
+the function and nowhere else
+
+global scope = we can reference an item anywhere
+in code
+ */
